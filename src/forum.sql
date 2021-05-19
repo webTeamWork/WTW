37895a04d09673c014183771ed08e54b0918d034
jpcli <jpcli888@gmail.com>
2021-05-19 11:32:26 +0800

    [Feature]初步建立数据库

---
+++ src/forum.sql
@@ -0,0 +1,119 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment`  (
+  `comment_id` int(11) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `comment_time` int(10) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`comment_id`) USING BTREE,
+  INDEX `topic_id`(`topic_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for record
+-- ----------------------------
+DROP TABLE IF EXISTS `record`;
+CREATE TABLE `record`  (
+  `record_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `record_type` tinyint(1) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `record_time` int(10) NOT NULL,
+  PRIMARY KEY (`record_id`) USING BTREE,
+  UNIQUE INDEX `user_id`(`user_id`, `record_type`, `topic_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for section
+-- ----------------------------
+DROP TABLE IF EXISTS `section`;
+CREATE TABLE `section`  (
+  `section_id` int(11) NOT NULL,
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `topic_count` int(11) NOT NULL,
+  PRIMARY KEY (`section_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tipoff
+-- ----------------------------
+DROP TABLE IF EXISTS `tipoff`;
+CREATE TABLE `tipoff`  (
+  `tip_id` int(11) NOT NULL,
+  `target_type` tinyint(1) NOT NULL,
+  `target_id` int(11) NOT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `tipoff_time` int(10) NOT NULL,
+  `process_type` tinyint(1) NOT NULL,
+  PRIMARY KEY (`tip_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for topic
+-- ----------------------------
+DROP TABLE IF EXISTS `topic`;
+CREATE TABLE `topic`  (
+  `topic_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `create_time` int(10) NOT NULL,
+  `comment_time` int(10) NOT NULL,
+  `section_id` int(11) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `comment_count` int(11) NOT NULL,
+  PRIMARY KEY (`topic_id`) USING BTREE,
+  INDEX `user_id`(`user_id`) USING BTREE,
+  FULLTEXT INDEX `title`(`title`)
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for topic_meta
+-- ----------------------------
+DROP TABLE IF EXISTS `topic_meta`;
+CREATE TABLE `topic_meta`  (
+  `meta_id` int(11) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `meta_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `meta_value` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`meta_id`) USING BTREE,
+  UNIQUE INDEX `topic_id`(`topic_id`, `meta_name`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `user _id` int(11) NOT NULL,
+  `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nickname` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `user_type` tinyint(1) NOT NULL,
+  `create_time` int(10) NOT NULL,
+  PRIMARY KEY (`user _id`) USING BTREE,
+  UNIQUE INDEX `email`(`email`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user_meta
+-- ----------------------------
+DROP TABLE IF EXISTS `user_meta`;
+CREATE TABLE `user_meta`  (
+  `meta_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `meta_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `meta_value` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`meta_id`) USING BTREE,
+  UNIQUE INDEX `user_id`(`user_id`, `meta_name`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
